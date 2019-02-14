f014d3f1f313db45fae7eb2b699e53ca1d3c5cbc
kurokyfan <kuro2007cumt@gamil.com>
2019-02-14 10:59:48 +0800

    init

---
+++ Day1/01.CustomFont/CustomFont/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CustomFont
+//
+//  Created by kuroky on 2019/2/14.
+//  Copyright © 2019 Emucoo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
